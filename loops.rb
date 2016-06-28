dfc44dcfb4cebfc7a3760036cfed4918a3bd63e6
maldon <maldonmeehan#gmail.com>
2016-06-27 21:13:54 -0700

    add loops file with instructions

---
+++ loops.rb
@@ -0,0 +1,9 @@
+
+
+
+# Play around with loops of your own now.
+#
+# Try removing the last 4 elements of an Array.
+# Add a number to itself several times.
+# Use a parameter with a times loop to create an Array that looks like this: [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4]
+# https://www.learnhowtoprogram.com/ruby/ruby-branching-and-looping/practice-loops
